69b47e22b61a68e07f65f173ad5ba9cca33be588
Prathamesh Bhat <prathameshtengse25@gmail.com>
2023-10-19 02:41:22 +0530

    pages added

---
+++ client/src/components/Layout/MainCorousal/MainCorousalData.js
@@ -0,0 +1,14 @@
+export const MainCorousalData = [
+  {
+    image: "https://watersanitationlearning.gov.in/public/images/1(9).jpg  ",
+  },
+  {
+    image: "https://watersanitationlearning.gov.in/public/images/1(5).jpg",
+  },
+  {
+    image: "https://watersanitationlearning.gov.in/public/images/1(4).jpg",
+  },
+  {
+    image:"https://sbmurban.org/storage/app/uploads/public/650/958/9cb/6509589cb915a012534176.jpeg",
+  },
+];
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import MainCorousal from "../components/Layout/MainCorousal/MainCorousal";
 
 const HomePage = () => {
   return (
     <Layout>
-      <h1>HomePage</h1>
+      <MainCorousal/>
     </Layout>
   );
 };
